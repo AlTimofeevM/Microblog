eaa086327ba28cb5ef8dba1c57a29334f56835b6
AlTimofeevM <al.timofeev.m@yandex.ru>
2020-02-27 20:59:41 +0300

    security

---
+++ src/main/java/org/altimofeevm/MyMicroblog/domain/Role.java
@@ -0,0 +1,5 @@
+package org.altimofeevm.MyMicroblog.domain;
+
+public enum Role {
+    USER;
+}
--- src/main/java/org/altimofeevm/MyMicroblog/repos/MessageRepo.java
+++ src/main/java/org/altimofeevm/MyMicroblog/repos/MessageRepo.java
@@ -9,5 +9,4 @@ import java.util.List;
 public interface MessageRepo extends CrudRepository<Message, Long> {
 
     List<Message> findByTag(String tag);
-
 }

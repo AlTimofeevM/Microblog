0e0beed72c33d2112a8b15b467a5cee5197991c5
AlTimofeevM <al.timofeev.m@yandex.ru>
2020-02-27 21:25:22 +0300

    OneToMany mapping

--- src/main/java/org/altimofeevm/MyMicroblog/domain/Role.java
+++ src/main/java/org/altimofeevm/MyMicroblog/domain/Role.java
@@ -1,5 +1,12 @@
 package org.altimofeevm.MyMicroblog.domain;
 
-public enum Role {
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
     USER;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
 }

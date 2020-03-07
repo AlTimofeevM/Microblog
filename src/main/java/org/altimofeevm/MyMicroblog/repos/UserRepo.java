3f97daae8317185844f141cdc2249de2a78d666d
AlTimofeevM <al.timofeev.m@yandex.ru>
2020-03-07 15:43:40 +0300

    mailsender

--- src/main/java/org/altimofeevm/MyMicroblog/repos/UserRepo.java
+++ src/main/java/org/altimofeevm/MyMicroblog/repos/UserRepo.java
@@ -3,6 +3,9 @@ package org.altimofeevm.MyMicroblog.repos;
 import org.altimofeevm.MyMicroblog.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepo extends JpaRepository<User, Long> {
     User findByUsername(String username);
+
+    User findByActivationCode(String code);
 }
